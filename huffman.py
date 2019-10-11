96204c8133fd666167ce6cd562ee4d6e1dd6dd71
Dan Menjivar <Daniel.quintanamenjivar882@myci.csuci.edu>
2019-10-11 09:30:16 -0700

    first commit

---
+++ huffman.py
@@ -0,0 +1,100 @@
+'''
+Algorithms HW #2
+Huffman
+Based on: https://www.youtube.com/watch?v=nr3bZdL0aCs 
+'''
+
+print('Huffman Encoding Program')
+file_name = input('Enter the name of a text file to open: ') + '.txt' #  prompt the user the name of a textfile
+
+file = open(file_name, 'r') 
+if file.mode == 'r':
+    contents = file.read() # read the contents of that text file
+
+letters = [] # make a list of characters with their corresponding frequencies (e.g. [3, 'g'])
+only_letters = [] # and a list of only the characters used
+for letter in contents: 
+    if letter not in letters:
+        freq = contents.count(letter)
+        letters.append(freq)
+        letters.append(letter)
+        only_letters.append(letter)
+
+temp_letters_with_frequency = list.copy(letters)  # this list is used to sort the codes later
+letters_with_frequency = []
+for i in range(0, len(temp_letters_with_frequency), 2):
+    new_element_for_letters_freq = [temp_letters_with_frequency[i], temp_letters_with_frequency[i + 1]]
+    letters_with_frequency.append(new_element_for_letters_freq)
+letters_with_frequency.sort(key=lambda x : x[0], reverse=True)
+nodes = [] # generate the base nodes for the Huffman tree as "[freq, letter] => [7, "e"]"
+while len(letters) > 0:
+    nodes.append(letters[0:2])
+    letters = letters[2:]
+nodes.sort()
+huffman_tree = []
+huffman_tree.append(nodes) # add all the nodes to the tree
+
+def combine(nodes): # recursively combine base nodes to create a huffman tree
+    pos = 0
+    newnode = []
+    if len(nodes) > 1: # grab the 2 lowest nodes
+        nodes.sort(key=lambda x : int(x[0]))
+        nodes[pos].append('0') #  add a 0 or 1
+        nodes[pos + 1].append('1')
+        combined_node1 = str(nodes[pos][0]) + str(nodes[pos + 1][0])
+        combined_node2 = str(nodes[pos][1]) + str(nodes[pos + 1][1])
+        newnode.append(combined_node1)
+        newnode.append(combined_node2)
+        newnodes = []
+        newnodes.append(newnode)
+        newnodes = newnodes + nodes[2:]
+        nodes = newnodes
+        huffman_tree.append(nodes)
+        combine(nodes)
+    return huffman_tree
+
+newnodes = combine(nodes)
+huffman_tree.sort(key = lambda x: str(x[0]), reverse=True) #  invert the tree to match hand drawn
+checklist = [] # remove any duplicates
+for level in huffman_tree:
+    for node in level:
+        if node not in checklist:
+            checklist.append(node)
+        else:
+            level.remove(node)
+#count = 0
+# for level in huffman_tree: #print huffman tree, commented out unnecessary for problem description
+#     print('Level', count, ':', level)
+#     count += 1
+
+# print()
+
+letter_binary = [] # build a table of the huffman codes
+if len(only_letters) == 1:
+    letter_code = [only_letters[0], '0']
+    letter_binary.append(letter_code * len(contents))
+else: 
+    for letter in only_letters:
+        lettercode = ''
+        for node in checklist:
+            if len(node) > 2 and letter in node[1]:
+                lettercode = lettercode + node[2]
+        letter_code = [letter, lettercode]
+        letter_binary.append(letter_code)
+
+lookup = {y : x for x, y in letters_with_frequency} # used to sort the huffman codes by frequency
+huffman_table_sorted = sorted(letter_binary, key=lambda x: lookup[x[0]], reverse=True) # sorts the huffman codes by frequency
+
+print('Binary codes are: ')
+print('Character\tBinary Huffman\tBinary ASCII')
+for i in range(len(huffman_table_sorted)):
+    print('\'%s\'\t\t%s\t\t%s' % (huffman_table_sorted[i][0], huffman_table_sorted[i][1], bin(ord(huffman_table_sorted[i][0][0]))[2:]))
+
+bitstring = '' #  create a bitstring of the original message using the created codes
+for character in contents:
+    for item in letter_binary:
+        if character in item:
+            bitstring = bitstring + item[1]
+
+print("The contents of the file are: \'%s\'" % contents) # print the original content
+print('The huffman encoded contents are: \'%s\'' % bitstring) # print the huffman coded content
